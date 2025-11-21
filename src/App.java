b16232b71b2ac75609d8c569b9e01de501b40dbc
123EFD <erchinqq06@gmail.com>
2025-11-21 23:06:29 +0800

    Commit changes

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 import java.net.URL;
 
-public class FXMLExample extends Application {
+public class App extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -18,20 +18,19 @@ public class FXMLExample extends Application {
         // IMPORTANT: Use getResource() to load the FXML file from the classpath.
         // This is the standard, portable way.
         // It assumes 'hello-world.fxml' is in the same directory as this Java file.
-        //URL fxmlUrl = getClass().getResource("hello-world.fxml");
+        URL fxmlUrl = getClass().getResource("hello-world.fxml");
 
-        /*if (fxmlUrl == null) {
+        if (fxmlUrl == null) {
             System.err.println("Error: Could not find hello-world.fxml on the classpath.");
             return; // Stop execution if the FXML file is missing
         }
-         */
 
-        FXMLLoader loader = new FXMLLoader();
+        FXMLLoader loader = new FXMLLoader(fxmlUrl);
         /*  The path uses forward slashes (/) and starts with file:///.
         You must use the full, absolute path to your file.
         Not recommended for production code, but useful for quick tests.
          */
-        loader.setLocation(new URL("file:///C:/Users/Chin Shi Er/OneDrive/Documents/Coding/demofx/src/com/jenkov/javafx/hello-world.fxml"));
+        //loader.setLocation(new URL("file:///C:/Users/Chin Shi Er/OneDrive/Documents/Coding/demofx/src/hello-world.fxml"));
         // Load the root container defined in the FXML
         VBox vbox = loader.<VBox>load();
 
